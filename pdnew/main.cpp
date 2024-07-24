7ee9048ae0e6c85fae47126c719672a97dd17974
Yevgeniy Zakharov <fnc12@me.com>
2024-07-25 00:24:59 +0500

    Initial Commit

---
+++ pdnew/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pdnew
+//
+//  Created by Yevgeniy Zakharov on 25.07.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
