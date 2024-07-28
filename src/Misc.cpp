f2f7ec23bb68ffc4a27fda037e2f6c1a330db42c
Yevgeniy Zakharov <fnc12@me.com>
2024-07-28 15:50:07 +0500

    added algorithm

--- src/Misc.cpp
+++ src/Misc.cpp
@@ -1,5 +1,7 @@
 #include "Misc.hpp"
 
+#include <algorithm>
+
 std::string removeAllSpaces(std::string_view stringView) {
     std::string result(stringView);
     result.erase(std::remove(result.begin(), result.end(), ' '), result.end());
